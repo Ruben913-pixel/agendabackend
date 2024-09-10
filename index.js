16749946b641141a6efa725aae094cb18f87dfb4
Ruben913-pixel <rubencastellanos913@gmail.com>
2024-09-11 01:24:54 +0200

    nueva agenda

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const cors= require("cors")
+
+app.use(express.json());
+app.use(cors())
+app.use(express.static('dist'))
+
+
+let number = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+
+app.get("/api/persons", (request, response) => {
+  response.status(200).json(number)
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const newNumber = number.find((number) => number.id === id);
+  if (!newNumber) {
+    return response.status(404).end();
+  }
+  response.json(newNumber);
+});
+
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const deleterNumber = number.filter((number) => number.id !== id);
+  number = deleterNumber;
+  response.json(number);
+});
+
+app.post("/api/persons", (resquest, response) => {
+  const body = resquest.body;
+ console.log(body)
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: number.length + 1,
+  };
+  number = [...number, newPerson];
+  response.json(number);
+});
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${pueto}`);
+});
