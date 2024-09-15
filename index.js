78d540b5ba00287a2ed1cafc69f78640b8be97f2
Ruben913-pixel <rubencastellanos913@gmail.com>
2024-09-15 22:46:46 +0200

    nueva base de datos

--- index.js
+++ index.js
@@ -2,33 +2,8 @@ const express = require("express");
 const app = express();
 const cors= require("cors")
 
-app.use(express.json());
 app.use(cors())
 app.use(express.static('dist'))
-
-
-let number = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 
 app.get("/api/persons", (request, response) => {
